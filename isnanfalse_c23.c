c9506c6d502003a4c51f6e363f10ffd92d5f006b
Anya Desdein <heavelyn1999@gmail.com>
2024-09-21 16:36:20 +0200

    Add checking if nan, pointer to null and 0 are false in c23

---
+++ isnanfalse_c23.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main() {
+	double a = NAN;
+	int *b = NULL;
+	int c = 0;
+	float d = 0;
+
+	printf("Checking if bool %d, %d, %d, %d \n", (bool)a, (bool)b, (bool)c, (bool)d);
+
+}
+
+
