c5d2732dd5561dd44a9410d64f44f5b8bb8755ef
Anya Desdein <heavelyn1999@gmail.com>
2024-09-23 23:01:59 +0200

    Add additional comments and better formatting for isnanfalse_c23.c

--- isnanfalse_c23.c
+++ isnanfalse_c23.c
@@ -8,7 +8,9 @@ int main() {
 	int c = 0;
 	float d = 0;
 
-	printf("Checking if bool %d, %d, %d, %d \n", (bool)a, (bool)b, (bool)c, (bool)d);
+	printf("Let's check if a NAN, pointer to NULL, 0 int and 0 float are in fact False\n");
+	printf("C23 ISO says that NAN will be true, consecutive results are presented in boolean:\n");
+	printf("%d, %d, %d, %d \n", (bool)a, (bool)b, (bool)c, (bool)d);
 
 }
 
