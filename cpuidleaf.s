8e4ec9cee595d48881c96ca24ce0025fb2acfd9a
Anya Desdein <heavelyn1999@gmail.com>
2024-09-24 17:43:55 +0200

    Fix cpudata overwrite issue Cpudata no longer overwritten due to allocating twice the location size Exit call optimized by using edi in xor Moved counter to .bss and allocated 1 byte for it, adjusted the code accordingly

--- cpuidleaf.s
+++ cpuidleaf.s
@@ -7,33 +7,32 @@
 
 
 section .bss
-	cpudata resb 12 ; reserve 12 bytes
-
-section .data
-	counter dq 17
+	cpudata resb 12 ; reserve 12 bytes for cpuid data
+	counter resb 1 ; reserve one byte for iterator
 
 section .text
 	global _start;
 _start:
-	mov eax, 0 ; see how many leafs can I query for
+	xor eax, eax ; see how many leafs can I query for
 	cpuid
 	inc eax
-	mov [counter], eax 
+	mov [counter], al 
 _loop:		
-	mov rax, [counter]
-	dec rax
-	mov [counter], rax
+	movzx rax, al
+	mov al, [counter]
+	dec al
+	mov [counter], al
 	cpuid
-	mov [cpudata], rbx
-	mov [cpudata + 4], rcx
-	mov [cpudata + 8], rdx
-	mov rcx, 1
-	mov rax, [counter]
+	mov [cpudata], ebx
+	mov [cpudata + 4], ecx
+	mov [cpudata + 8], edx
+	mov ecx, 1
+	mov al, [counter]
 	cmp rax, rcx
 	jg _loop
 	
 
 _exit: 
 	mov rax, 60 ; exit call
-	xor rdi, rdi
+	xor edi, edi ; this syscall checks only lower 32 bits
 	syscall
