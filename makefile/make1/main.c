ec1c0a5d459e3d04c92002225b0dfd379c3c86d6
Anya Desdein <heavelyn1999@gmail.com>
2025-01-24 17:35:30 +0100

    Add cleanup and seek_end
    Remove the file leakage issue
    Fix minor things

--- makefile/make1/main.c
+++ makefile/make1/main.c
@@ -5,27 +5,43 @@
 
 #define BUFFER_SIZE 1024
 
+void clfl(FILE **ptr) {
+	fclose(*ptr);
+}
+
+int is_ascii(char c) {
+	return c == '\n' || (c > 31 && c < 127);
+}
+
 int main(int argc, char *argv[]) {
 	if (argc != 3) {
-		perror("Incorrect number of arguments. 1 - path to a file, 2 - text to append");
+		printf("Usage: %s FILE_PATH TEXT_TO_APPEND\n", argv[0]);
 		return 1;
 	}
 
-	FILE *file = fopen(argv[1], "r+");
+	FILE *file __attribute__((cleanup(clfl))) = fopen(argv[1], "r+");
 	if (file == NULL) {
-		perror("Incorrect file path.");
+		perror("file open");
 		return 1;
 	}
-	
+
 	char buffer[BUFFER_SIZE];
 	int position = 0;
-	
+
 	while (1) {
-		size_t buf_len = fread(buffer, 1, BUFFER_SIZE, file);
+		size_t buf_len = fread(buffer, 1, BUFFER_SIZE-1, file);
 		if (buf_len == 0) {
 			break;
 		}
 
+		for (size_t i=0; i<buf_len; i++) {
+			if ( !is_ascii(buffer[i]) ) {
+				printf("Incorrect file contents: non-ascii %c symbol number %lu detected\n", buffer[i], (position+i));
+				return 1;
+			}
+		}
+
+		buffer[buf_len] = '\0';
 		printf(buffer);
 		
 		if (buf_len < BUFFER_SIZE) {
@@ -35,12 +51,11 @@ int main(int argc, char *argv[]) {
 		position += BUFFER_SIZE;
 	}	
 	
-	fseek(file, position, SEEK_SET);
-	fputs(argv[2], file);
+	fseek(file, 0, SEEK_END);
 
+	fputs(argv[2], file);
 	printf("\n");	
 	printf("Position: %d\n", position);
 	
-	fclose(file);
 	return 0;
 }
