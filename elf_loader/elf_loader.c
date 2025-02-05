fba82b3ce35cdab33e3e70fd2546d3e0de519e64
Anya Desdein <heavelyn1999@gmail.com>
2025-02-05 13:20:53 +0100

    Smol changes, no func change
    Changes:
    - E_indentifier struct defined
    - Cleaned up code
    - No multiple printfs anymore with longer files
    - Checking file len at the start
    - Variable renaming

--- elf_loader/elf_loader.c
+++ elf_loader/elf_loader.c
@@ -1,13 +1,14 @@
-#include  <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include  <errno.h>
+#include <string.h>
+#include <errno.h>
 
 #include <fcntl.h>
 #include <unistd.h>
 
 
 void clfl(int *file_descr) {
-	printf("Closing elf file, descriptor number %d\n", *file_descr);
+	printf("Closing file descriptor number %d\n", *file_descr);
 	close(*file_descr);
 }
 
@@ -28,34 +29,56 @@ int main(int argc, char *argv[]) {
 
 	char buffer[BUFFER_SIZE];
 	ssize_t bytes_read;
-	int iter = 0;
-	int starts_with_elf = 0;
+	struct E_ident {
+		unsigned char el_mag0;	
+		unsigned char el_mag1;
+		unsigned char el_mag2;
+		unsigned char el_mag3;
+		unsigned char el_class;
+		unsigned char el_data;
+		unsigned char el_version;
+		unsigned char el_osabi;
+		unsigned char el_abiversion;
+		unsigned char el_pad;
+		unsigned char el_nident;
+	} E_ident;
+	unsigned char e_magic[] = {0x7f, 0x45, 0x4c, 0x46}; 
+
+	off_t buf_iter = 0;
+	off_t f_len = lseek(file_descr, 0, SEEK_END);
+	lseek(file_descr, 0, SEEK_SET);
+	printf("File len: %lld\n", (long long)f_len);
+	int is_magic = 0;
+	
 	while ((bytes_read = read(file_descr, buffer, BUFFER_SIZE)) > 0) {
-		if (iter == 0) {
-			unsigned char elf_hex[] = {0x7f, 0x45, 0x4c, 0x46};
-			for (int i=0; i<(sizeof(elf_hex)/sizeof(elf_hex[0])); i++) {
+		if (buf_iter == 0) {
+			for (int i=0; i<(sizeof(e_magic)/sizeof(e_magic[0])); i++) {
 				printf("%c", buffer[i]);
-				if(elf_hex[i] != buffer[i]) {
+				
+				if(e_magic[i] != buffer[i]) {
 					printf("\n");
 					break;
 				}
+				is_magic = 1;
+			}
+			
+			if (!is_magic) {
+				puts("Not an elf");
+				break;
+			}
+	
+			if(is_magic) {
 				printf("\n");
-				starts_with_elf = 1;
+				puts("It's an elf!");
 			}
 		}
-			
-		if (starts_with_elf == 0) {
-			puts("Not an elf");
-		} else {
-			puts("It's an elf!");
+		
+		if (buf_iter >= f_len) {	
+			break;
 		}
 
-		iter += 1024;
-		lseek(file_descr, iter, SEEK_SET );
+		buf_iter += 1024;
+		lseek(file_descr, buf_iter, SEEK_SET);
 	}
-	printf("File end\n");
-
-
-
 	return 0;
 }
