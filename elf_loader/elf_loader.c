2d72515d0de4b4efc2f55f96e67330a471262289
Anya Desdein <heavelyn1999@gmail.com>
2025-02-07 18:36:28 +0100

    Header display total rewrite

--- elf_loader/elf_loader.c
+++ elf_loader/elf_loader.c
@@ -5,7 +5,8 @@
 
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
 
 void clfl(int *file_descr) {
 	printf("Closing file descriptor number %d\n", *file_descr);
@@ -13,7 +14,7 @@ void clfl(int *file_descr) {
 }
 
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 64
 
 int main(int argc, char *argv[]) {
 	if (argc !=2) {
@@ -27,11 +28,8 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-
-
 	char buffer[BUFFER_SIZE];
 	ssize_t bytes_read;
-
 
 	typedef struct {
 		unsigned char el_mag0;	
@@ -43,95 +41,94 @@ int main(int argc, char *argv[]) {
 		unsigned char el_version;
 		unsigned char el_osabi;
 		unsigned char el_abiversion;
+		char el_pad[7];
 	} E_ident_t;
 
 	union E_ident_u {
 		E_ident_t as_struct;
-		unsigned char as_bytes[9];
+		unsigned char as_bytes[sizeof(E_ident_t)];
 	};
 	
+	_Static_assert(sizeof(E_ident_t) == 16, "e_ident not 16 bytes");
+
 	union E_ident_u E_ident_i;
-
-
 	unsigned char e_magic[] = {0x7f, 0x45, 0x4c, 0x46}; 
 
-	off_t buf_iter = 0;
-	off_t f_len = lseek(file_descr, 0, SEEK_END);
-	lseek(file_descr, 0, SEEK_SET);
-	printf("File len: %lld\n", (long long)f_len);
-	int is_magic = 0;
-	
-	while ((bytes_read = read(file_descr, buffer, BUFFER_SIZE)) > 0) {
-		if (buf_iter == 0) {
-			for (int i=0; i<(sizeof(e_magic)/sizeof(e_magic[0])); i++) {
-				printf("%c", buffer[i]);
-				
-				if(e_magic[i] != buffer[i]) {
-					printf("\n");
-					break;
-				}
-				is_magic = 1;
-			}
-			
-			if (!is_magic) {
-				puts("Not an elf");
-				break;
-			}
-			puts("");
-
-			memcpy(&E_ident_i.as_bytes[0], &buffer, sizeof(E_ident_i));
-			memcpy(&E_ident_i.as_struct, &buffer, sizeof(E_ident_i));
-			
-			// Class
-			if (E_ident_i.as_bytes[4] > 2 || E_ident_i.as_bytes[4] < 1 ) {
-				printf("Unknown ELF Class %02x\n", E_ident_i.as_bytes[4]);
-			} else if (E_ident_i.as_bytes[4] == 2) {
-				printf("ELF 64-bit\n");
-			} else if (E_ident_i.as_bytes[4] == 1) {
-				printf("ELF 32-bit\n");
-			}
-
-			// Data Encoding
-			if (E_ident_i.as_bytes[5] > 2 || E_ident_i.as_bytes[5] < 1 ) {
-				printf("Unknown Data Encoding %02x\n", E_ident_i.as_bytes[5]);
-			} else if (E_ident_i.as_bytes[5] == 2) {
-				printf("Big-endian\n");
-			} else if (E_ident_i.as_bytes[5] == 1) {
-				printf("Little-endian\n");
-			}
-			
-			// Version
-			printf("File version: %02x\n", E_ident_i.as_bytes[6]);
-
-
-			// Operating System & ABI
-			if (E_ident_i.as_bytes[7] == 0) {
-				printf("System V ABI\n");
-			} else if (E_ident_i.as_bytes[7] == 1) {
-				printf("HP-UX OS\n");
-			} else if (E_ident_i.as_bytes[7] == 255) {
-				printf("Standalone (embedded) application\n");
-			} else {
-				printf("Unknown OS & ABI %02x\n", E_ident_i.as_bytes[7]);
-			}
-
-			// ABI Version
-			printf("ABI Version: %02x\n", E_ident_i.as_bytes[8]);
-
-
-
-
-
-		}
-		
-		if (buf_iter >= f_len) {	
-			break;
-		}
-
-		buf_iter += 1024;
-		lseek(file_descr, buf_iter, SEEK_SET);
-	
+	struct stat file_status;
+	int fstat_chck = fstat(file_descr, &file_status);
+	if (fstat_chck == -1) {
+		perror("Fstat failure");
 	}
+	printf("File size: %lld\n", (long long)file_status.st_size);
+	
+	off_t curr_br;
+	for (off_t bytes_read=0; bytes_read < 64; ) {
+		curr_br = read(file_descr, buffer + bytes_read, (BUFFER_SIZE - bytes_read));
+		if (curr_br == -1) {
+			printf("Error reading file\n");
+			return 1;
+		}
+
+		if (curr_br == 0) {
+			printf("Unexpected EoF\n");
+			return 1;
+		}
+
+		bytes_read += curr_br;
+	}
+	
+	memcpy(&E_ident_i.as_bytes[0], &buffer, sizeof(E_ident_i));
+	memcpy(&E_ident_i.as_struct, &buffer, sizeof(E_ident_i));
+		
+	for (int i=0; i<(sizeof(e_magic)/sizeof(e_magic[0])); i++) {
+		printf("%c", E_ident_i.as_bytes[i]);
+				
+		if(e_magic[i] != E_ident_i.as_bytes[i]) {
+			puts("");
+			printf("Not an Elf: No Magic\n");
+			return 1;		
+		}
+	}
+	puts("");
+	
+	// Class
+	if (E_ident_i.as_bytes[4] > 2 || E_ident_i.as_bytes[4] < 1 ) {
+		printf("Unknown ELF Class %02x\n", E_ident_i.as_bytes[4]);
+		return 1;
+	} else if (E_ident_i.as_bytes[4] == 2) {
+		printf("ELF 64-bit\n");
+	} else if (E_ident_i.as_bytes[4] == 1) {
+		printf("ELF 32-bit\n");
+	}
+
+	// Data Encoding
+	if (E_ident_i.as_bytes[5] > 2 || E_ident_i.as_bytes[5] < 1 ) {
+		printf("Unknown Data Encoding %02x\n", E_ident_i.as_bytes[5]);
+		return 1;
+	} else if (E_ident_i.as_bytes[5] == 2) {
+		printf("Big-endian\n");
+	} else if (E_ident_i.as_bytes[5] == 1) {
+		printf("Little-endian\n");
+	}
+			
+	// Version
+	printf("File version: %02x\n", E_ident_i.as_bytes[6]);
+
+
+	// Operating System & ABI
+	if (E_ident_i.as_bytes[7] == 0) {
+		printf("System V ABI\n");
+	} else if (E_ident_i.as_bytes[7] == 1) {
+		printf("HP-UX OS\n");
+	} else if (E_ident_i.as_bytes[7] == 255) {
+		printf("Standalone (embedded) application\n");
+	} else {
+		printf("Unknown OS & ABI %02x\n", E_ident_i.as_bytes[7]);
+		return 1;
+	}
+
+	// ABI Version
+	printf("ABI Version: %02x\n", E_ident_i.as_bytes[8]);
 
 	return 0;
 }
