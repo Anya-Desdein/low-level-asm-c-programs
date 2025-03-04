5efacdacb8c5b21f84208f1a8db7b1a5c321f87b
Anya-Desdein <heavelyn1999@gmail.com>
2025-03-04 23:28:52 +0100

    Add overcomplicated macro
    There's no reason in heaven nor hell to use this
    since we already have static assert
    
    BUT this is an interesting trick to check array size,
    at the same time fail at compile time if pointer
    and avoid some problems with pointers
    that normal people have.
    
    On the other hand, you have 3 more problems:
    - BUILD_BUG_ON_ZERO which does return zero, NOT FAIL ON 0
    it's a thing from linux, ask them why they did this
    - __same_type
    - ARRAY_SIZE
    
    It took me a while to understand tbh
    It uses incorrect bit-field size to fail at compile time
    The name is confusing
    I've spent half an hour thinking it's incorrect
    
    TODO: Actually use it in my program outside of a test

--- cpp/random/off_on_cpus.c
+++ cpp/random/off_on_cpus.c
@@ -9,6 +9,41 @@
 #include <inttypes.h>
 
 #include <linux/limits.h>
+
+/* This is complicated... and stolen from Linux
+In C, bit-field size must be non-negative
+On positive, we're turning it to -1
+On 0, it stays 0 */
+#define BUILD_BUG_ON_ZERO(expr) ((int)(sizeof(struct { int:(-!!(expr)); })))
+
+/* Are two types/vars the same (ignoring qualifiers)? 
+^ Stolen from linux compiler_types.h :P */
+
+/* If two values are of the same type, return 1, else 0  */
+#define __same_type(a,b) __builtin_types_compatible_p(typeof(a), typeof(b))
+
+
+
+/* Using macros above, 
+If arr[x], it will end up as
+0, ARR_EL_COUNT
+But if it's a pointer, it will end up as
+1...
+and fail at compile time.
+
+Due to comma operator, it will ignore the first arg and
+pass the second one as the result.
+
+Without the first argument it would be
+, ARR_EL_COUNT
+Which, on the other hand, is incorrect syntax and wouldn't work.
+*/
+#define ARRAY_SIZE(arr) \
+	(BUILD_BUG_ON_ZERO(\
+		__same_type((arr), &(arr)[0])), \
+		(sizeof(arr) / sizeof((arr)[0])) \
+	)
+
 int pleaseleave() {	
 	printf("Usage:\n\toocpu list,\n\toocpu on CPU_NUM,\n\toocpu off CPU_NUM\n");
 	return 1;
@@ -21,6 +56,10 @@ int plsplspls(int cpus) {
 
 int main(int argc, char *argv[]) {
 	
+	// Testing ARRAY_SIZE
+	int tab[12];
+	printf("%d\n", ARRAY_SIZE(tab));
+
 	if (argc < 2 || argc > 3) {
 		return pleaseleave();
 	}
@@ -38,13 +77,12 @@ int main(int argc, char *argv[]) {
 	}
 
 	// Create correct path to a file managing core state (on/off)
-	int cpu_num = atoi(argv[2]);
-	cpu_num --;
+	int cpu_num = atoi(argv[2]) - 1;
 
 	char str[] = "/sys/devices/system/cpu/cpu%d/online";
 	char buffer[PATH_MAX];
 
-	int cx = sprintf(buffer, str, cpu_num);
+	sprintf(buffer, str, cpu_num);
 	// printf("Path to file being edited: %s\n", buffer);
 
 	if (strcmp(argv[1],"on") == 0) {
