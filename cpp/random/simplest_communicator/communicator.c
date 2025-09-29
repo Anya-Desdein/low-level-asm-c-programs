2148bbb10cfaa60e7e046f202804842597c9e666
Anya-Desdein <heavelyn1999@gmail.com>
2025-09-29 12:14:49 +0200

    Bind a socket

--- cpp/random/simplest_communicator/communicator.c
+++ cpp/random/simplest_communicator/communicator.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/un.h>
+
+#define SOCKET_PATH "/tmp/socket_test"
 
 int main(void) {
 	int fd = socket(AF_UNIX, SOCK_STREAM, 0);
@@ -8,10 +11,21 @@ int main(void) {
 		perror("socket");
 		return 1;
 	}
+	printf("%d\n", fd);
+	unlink(SOCKET_PATH);
 
-	
+	struct sockaddr_un server_addr;
 
+	memset(&server_addr, 0,sizeof(server_addr));
+	server_addr.sun_family = AF_UNIX;
+	strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path));
 
+	if(bind(fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) ==  -1) {
+		perror("bind");
+		close(fd);
+		return 1;
+
+	}
 
 	close(fd);
 	return 0;
